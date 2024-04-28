68dcea51432ea0b3fd2d6c108a5807ec0a3c5428
marimari-88 <163653710+marimari-88@users.noreply.github.com>
2024-04-28 09:09:57 +0330

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,8 +17,8 @@ export default function Home() {
           >
             By{" "}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
+              src="/kabirkooh.svg"
+              alt="kabirkooh Logo"
               className="dark:invert"
               width={100}
               height={24}
